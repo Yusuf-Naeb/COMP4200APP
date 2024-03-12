adcdaa8770d5e59be5a17e2e892530de0f1951ee
Yusuf Naebkhil <yusuf.naeb@outlook.com>
2024-03-12 12:35:26 -0400

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "COMP4200Group"
+include(":app")
+ 

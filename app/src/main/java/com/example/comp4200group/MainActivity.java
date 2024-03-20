48e4ede013285290d1eaea37f906098b29a02146
UDH6 <163171887+UDH6@users.noreply.github.com>
2024-03-20 18:57:43 -0400

    Testing inserting new entries into database

--- app/src/main/java/com/example/comp4200group/MainActivity.java
+++ app/src/main/java/com/example/comp4200group/MainActivity.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Database_Func dbhandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         dbhandler = new Database_Func(MainActivity.this);
+         dbhandler.addUser("test", "test", "test");
+         dbhandler.addMood("happy", null, 1);
         //test
     }
 }

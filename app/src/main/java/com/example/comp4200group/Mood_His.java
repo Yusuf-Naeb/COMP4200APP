a89cd0f5ad333817eac0ce26c618994aa4b20bd9
UDH6 <163171887+UDH6@users.noreply.github.com>
2024-03-20 18:55:01 -0400

    Classes to hold data related to database tables

---
+++ app/src/main/java/com/example/comp4200group/Mood_His.java
@@ -0,0 +1,57 @@
+package com.example.comp4200group;
+
+public class Mood_His {
+    private int UserID;
+    private int MoodID;
+    private String Mood;
+    private String Mood2;
+    private String Date;
+    //getters
+    public String getMood() {
+        return Mood;
+    }
+
+    public String getMood2() {
+        return Mood2;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public int getUserID() {
+        return UserID;
+    }
+
+    public int getMoodID() {
+        return MoodID;
+    }
+    //setters
+    public void setMood(String mood) {
+        Mood = mood;
+    }
+
+    public void setMood2(String mood2) {
+        Mood2 = mood2;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public void setUserID(int userID) {
+        UserID = userID;
+    }
+
+    public void setMoodID(int moodID) {
+        MoodID = moodID;
+    }
+    //constructor
+    public Mood_His(int UserID, int MoodID, String Mood, String Mood2, String Date){
+        this.UserID = UserID;
+        this.MoodID = MoodID;
+        this.Mood = Mood;
+        this.Mood2 = Mood2;
+        this.Date = Date;
+    }
+}
---
+++ app/src/main/java/com/example/comp4200group/User.java
@@ -0,0 +1,48 @@
+package com.example.comp4200group;
+
+public class User {
+    private String Username;
+    private String Password;
+    private int UserID;
+    private String Name;
+
+    //getters
+    public String getName() {
+        return Name;
+    }
+
+    public int getUserID() {
+        return UserID;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+    //setters
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public void setUserID(int userID) {
+        UserID = userID;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+    //constructor
+    public User(String Username, String Password, int UserID, String Name){
+        this.Username = Username;
+        this.Password = Password;
+        this.Name = Name;
+        this.UserID = UserID;
+    }
+}

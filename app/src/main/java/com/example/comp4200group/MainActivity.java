c7a777f9acc7c347903c3efaf1417a6330ff7305
Justin4723 <147115955+Justin4723@users.noreply.github.com>
2024-03-22 14:19:01 -0400

    Update MainActivity.java
    Added activity for login

--- app/src/main/java/com/example/comp4200group/MainActivity.java
+++ app/src/main/java/com/example/comp4200group/MainActivity.java
@@ -1,16 +1,38 @@
 package com.example.comp4200group;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.View;
+
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     private Database_Func dbhandler;
+    private EditText usernameET;
+    private EditText passwordET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usernameET = findViewById(R.id.username);
+        passwordET = findViewById(R.id.password);
+
+        Button login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //retrieve text from the editText
+                String username = usernameET.getText().toString();
+                String password = passwordET.getText().toString();
+
+                //Some sort of authenticator method
+            }
+        });
+
          dbhandler = new Database_Func(MainActivity.this);
          dbhandler.addUser("test", "test", "test");
          dbhandler.addMood("happy", null, 1);
